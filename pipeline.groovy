c3ac2efa9de9cb459870a9c267a7f65e82b4cd34
amit kambli <amitmkambli@outlook.com>
2024-05-28 18:08:49 +0530

    added post section in pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -18,23 +18,22 @@ pipeline {
                 bat " mvn clean test -Dbrowser=firefox"
             }
         }
-        stage('TestNG Report') {
-            steps {
-                // To publish testng report
-                testNG()
-            }
-        }
         stage('Publish Extent Report') {
             steps {
-                publishHTML([allowMissing: false, alwaysLinkToLastBuild: true, keepAll: false, reportDir: '', reportFiles: 'reports/Extent_*.html', reportName: 'PipelineReport', reportTitles: 'ExtentReport', useWrapperFileDirectly: true])
+                publishHTML([allowMissing: false, alwaysLinkToLastBuild: true, keepAll: false, reportDir: '', reportFiles: 'reports/Extent_*.html', reportName: 'PipelineReport', reportTitles: '', useWrapperFileDirectly: true])
             }
         }
         stage('Send Email') {
             steps {
-                emailext body: '$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS: \nCheck console output at $BUILD_URL to view the results.',
+                emailext body: '$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS <br>Check console output at $BUILD_URL to view the results.',
                 subject: '$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS!',
                 to: 'xyz210716@gmail.com'
             }
+        }
+    }
+    post {
+        always {
+            testNG()
         }
     }
 }
